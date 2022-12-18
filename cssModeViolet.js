c08dc1590987619c0d7b059e4b656c22c6946d7c
Léo Oger <114417790+Magikscubs@users.noreply.github.com>
2022-12-18 17:00:25 +0100

    Update cssModeViolet.js

--- cssModeViolet.js
+++ cssModeViolet.js
@@ -1,7 +1,7 @@
 function cssMode(){
     var theme = document.getElementsByTagName('link')[0]; 
     if (theme.getAttribute('href') == 'styleViolet.css') { 
-        theme.setAttribute('href', 'styleSombreViolet.css'); 
+        theme.setAttribute('href', 'styleSombre.css'); 
     } else { 
         theme.setAttribute('href', 'styleViolet.css'); 
     } 
